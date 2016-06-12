453fb177cbf5d7d61117f2dd5f30a76a0cafe60e
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-06-12 11:14:41 -0400

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,19 @@
 module SongsHelper
 
+  def artist_select(song, artist)
+    if song.artist.nil?
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+    else
+      display_artist(song)
+      hidden_field_tag "song[artist_id]", song.artist_id
+    end
+  end
+  # what was this artist variable for??
+  def display_artist(song)
+    if song.artist.nil?
+      link_to('Add Artist', edit_song_path(song))      
+    else
+      link_to(song.artist.name, artist_path(song.artist))
+    end
+  end
 end
